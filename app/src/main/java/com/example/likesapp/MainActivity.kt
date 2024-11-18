a9ba947e6837117727b1e689cdf8d079ed7e6441
Muhammad Nurfadhli <79645149+mozart-69@users.noreply.github.com>
2024-11-18 16:04:29 +0700

    perbarui tampilan

--- app/src/main/java/com/example/likesapp/MainActivity.kt
+++ app/src/main/java/com/example/likesapp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.likesapp
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.Path
 import android.graphics.RectF
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -26,9 +26,6 @@ class MainActivity : AppCompatActivity() {
     private val right = mBitmap.width - left
     private val bottom = mBitmap.height.toFloat() - 50F
 
-    private val message = "Apakah kamu suka bermain?"
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -36,35 +33,25 @@ class MainActivity : AppCompatActivity() {
 
         binding.imageView.setImageBitmap(mBitmap)
 
-        showText()
-
         binding.like.setOnClickListener {
+            showEars()
             showFace()
             showMouth(true)
             showEyes()
+            showNose()
+            showHair()
             binding.imageView.invalidate()
         }
 
         binding.dislike.setOnClickListener {
+            showEars()
             showFace()
             showMouth(false)
             showEyes()
+            showNose()
+            showHair()
             binding.imageView.invalidate()
         }
-    }
-
-    private fun showText() {
-        val mPaintText =  Paint(Paint.FAKE_BOLD_TEXT_FLAG).apply {
-            textSize = 50F
-            color = ResourcesCompat.getColor(resources, R.color.black , null)
-        }
-
-        val mBounds = Rect()
-        mPaintText.getTextBounds(message, 0, message.length, mBounds)
-
-        val x: Float = halfOfWidth - mBounds.centerX()
-        val y = 50F
-        mCanvas.drawText(message, x, y, mPaintText)
     }
 
     private fun showFace() {
@@ -86,6 +73,48 @@ class MainActivity : AppCompatActivity() {
         mPaint.color = ResourcesCompat.getColor(resources, R.color.white, null)
         mCanvas.drawCircle(halfOfWidth - 120F, halfOfHeight - 20F, 15F, mPaint)
         mCanvas.drawCircle(halfOfWidth + 80F, halfOfHeight - 20F, 15F, mPaint)
+    }
+
+    private fun showNose() {
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.black, null)
+        mCanvas.drawCircle(halfOfWidth - 40F, halfOfHeight + 140F, 15F, mPaint)
+        mCanvas.drawCircle(halfOfWidth + 40F, halfOfHeight + 140F, 15F, mPaint)
+    }
+
+    private fun showEars() {
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.brown_left_hair, null)
+        mCanvas.drawCircle(halfOfWidth - 300F, halfOfHeight - 100F, 100F, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.brown_right_hair, null)
+        mCanvas.drawCircle(halfOfWidth + 300F, halfOfHeight - 100F, 100F, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.red_ear, null)
+        mCanvas.drawCircle(halfOfWidth - 300F, halfOfHeight - 100F, 60F, mPaint)
+        mCanvas.drawCircle(halfOfWidth + 300F, halfOfHeight - 100F, 60F, mPaint)
+    }
+
+    private fun showHair() {
+        mCanvas.save()
+
+        val path = Path()
+
+        path.addCircle(halfOfWidth - 100F, halfOfHeight - 10F, 150F, Path.Direction.CCW)
+        path.addCircle(halfOfWidth + 100F, halfOfHeight - 10F, 150F, Path.Direction.CCW)
+
+        val mouth = RectF(halfOfWidth - 250F, halfOfHeight, halfOfWidth + 250F, halfOfHeight + 500F)
+        path.addOval(mouth, Path.Direction.CCW)
+
+        mCanvas.clipOutPath(path)
+
+        val face = RectF(left, top, right, bottom)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.brown_left_hair, null)
+        mCanvas.drawArc(face, 90F, 180F, false, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.brown_right_hair, null)
+        mCanvas.drawArc(face, 270F, 180F, false, mPaint)
+
+        mCanvas.restore()
     }
 
     private fun showMouth(isHappy: Boolean) {
